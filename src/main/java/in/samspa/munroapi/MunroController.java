3d8949d3f75b7d507beb4241a7143193ed371341
sam <sam@samspain.com>
2021-08-30 23:38:57 +0100

    Return mocked JSON with service stub

--- src/main/java/in/samspa/munroapi/MunroController.java
+++ src/main/java/in/samspa/munroapi/MunroController.java
@@ -13,10 +13,15 @@ import java.util.List;
 @RestController
 public class MunroController {
 
+    private final MunroService munroService;
+
+    public MunroController(MunroService munroService) {
+        this.munroService = munroService;
+    }
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Munro> getData() {
-        Munro munro = new Munro("Sam", 2.5D, "MUN", "000112");
-        return Arrays.asList(munro, munro);
+        return munroService.findData();
     }
 
 }
--- src/test/java/in/samspa/munroapi/MunroControllerTest.java
+++ src/test/java/in/samspa/munroapi/MunroControllerTest.java
@@ -2,6 +2,7 @@ package in.samspa.munroapi;
 
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -11,27 +12,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.hamcrest.Matchers.*;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@WebMvcTest(MunroController.class)
 class MunroControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private MunroService munroService;
+
     @Test
     void getData() throws Exception {
+        Munro firstMunro = new Munro("Sam", 2.5D, "MUN", "000112");
+        Munro secondMunro = new Munro("Sofia", 3.5D, "TOP", "223110");
+        when(munroService.findData()).thenReturn(Arrays.asList(firstMunro, secondMunro));
+
         mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].name", is("Sam")))
-                .andExpect(jsonPath("$[0].height", is(2.5D)))
-                .andExpect(jsonPath("$[0].category", is("MUN")))
-                .andExpect(jsonPath("$[0].gridReference", is("000112")));
+                .andExpect(jsonPath("$[0].name", is(firstMunro.getName())))
+                .andExpect(jsonPath("$[0].height", is(firstMunro.getHeight())))
+                .andExpect(jsonPath("$[0].category", is(firstMunro.getCategory())))
+                .andExpect(jsonPath("$[0].gridReference", is(firstMunro.getGridReference())))
+                .andExpect(jsonPath("$[1].name", is(secondMunro.getName())))
+                .andExpect(jsonPath("$[1].height", is(secondMunro.getHeight())))
+                .andExpect(jsonPath("$[1].category", is(secondMunro.getCategory())))
+                .andExpect(jsonPath("$[1].gridReference", is(secondMunro.getGridReference())));
     }
 
 }
